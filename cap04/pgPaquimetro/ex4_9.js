85199b7a16a31743ce9af8de0ea26c9d46866710
gaabrielarthur <gabrielarthur01mg@gmail.com>
2026-07-31 08:11:04 -0300

    Primeiro commit

---
+++ cap04/pgPaquimetro/ex4_9.js
@@ -0,0 +1,10 @@
+/* elabore um porgrama para simular um parquimetro, qual leia o valor depositado em um terminal de estacionamento
+rotativo. op programa deve informao tempo de permanencia do veiculo no local e o troco (se existir)
+se o valor for inferior ao tempo minimo exiba a mensagem "valor insuficiente"
+valores 
+1 real 30 min
+1,75 60 min
+3 reais 120 min 
+
+*/
+
